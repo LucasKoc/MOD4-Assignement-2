422c2950fa99fe0bfbdc6b0baeed543ec0789750
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-09-06 14:29:30 -0400

    Exercice 3

--- src/PasswordVerifier/Main.java
+++ src/PasswordVerifier/Main.java
@@ -1,6 +1,31 @@
+/**
+ * The password should be at least six characters long.
+ * The password should contain at least one uppercase and at least one lowercase letter.
+ * The password should have at least one digit.
+ *
+ * @author SOLAN Tao & KOCOGLU Lucas
+ * @version N/A
+ * @since 2023-09-06
+ */
 package PasswordVerifier;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        PasswordVerifier pwd = new PasswordVerifier();
+        do {
+            System.out.println("--------------------------" +
+                    "\nPlease enter a strong password." +
+                    "\nStrong password is defined by :" +
+                    "\n\t> Six characters long a least" +
+                    "\n\t> Contain at least one uppercase letter" +
+                    "\n\t> Contain at least one lowercase letter" +
+                    "\n\t> Contain at least on digit." +
+                    "\n--------------------------" +
+                    "\nYour password :");
+            pwd.setStr(myScan.nextLine());
+        } while(!pwd.verifyCriteria());
     }
 }
--- src/PasswordVerifier/PasswordVerifier.java
+++ src/PasswordVerifier/PasswordVerifier.java
@@ -1,4 +1,59 @@
 package PasswordVerifier;
 
 public class PasswordVerifier {
+    private String pwd;
+
+    public PasswordVerifier() {
+        this.pwd = "";
+    }
+
+    public PasswordVerifier(String str) {
+        this.pwd = str;
+    }
+
+    public String getStr() {
+        return this.pwd;
+    }
+
+    public void setStr(String str) {
+        this.pwd = str;
+    }
+
+    public boolean isSixLong(String pwd) {
+        return pwd.length() >= 6;
+    }
+
+    public boolean isLowerCase(String pwd) {
+        for(int i = 0; i < pwd.length() ; i++) {
+            if (Character.isLowerCase(pwd.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    public boolean isUpperCase(String pwd) {
+        for(int i = 0; i < pwd.length() ; i++) {
+            if (Character.isUpperCase(pwd.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    public boolean isDigit(String pwd) {
+        for(int i = 0; i < pwd.length() ; i++) {
+            if (Character.isDigit(pwd.charAt(i))) return true;
+        }
+        return false;
+    }
+
+    public boolean verifyCriteria() {
+        boolean isSixLong = isSixLong(this.pwd), isLowerCase = this.isLowerCase(this.pwd), isUpperCase = this.isUpperCase(this.pwd), isDigit = this.isDigit(this.pwd);
+        System.out.println(isSixLong + " " + isLowerCase + " " + isUpperCase + " " + isDigit);
+        return isSixLong && isLowerCase && isUpperCase && isDigit;
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordVerifier{" +
+                "str='" + this.pwd + '\'' +
+                '}';
+    }
 }
