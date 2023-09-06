2a59b997db4012fce99dbcef742ba985eac18764
Tao Solan <tao.solan@gmail.com>
2023-09-06 11:10:47 -0400

    added setter for person

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -11,12 +11,24 @@ public class Person {
         this.phone = phone;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
     public String getName(){
         return this.name;
     }
 
+    public void setAddress(String address){
+        this.address = address;
+    }
+
     public String getAddress(){
         return this.address;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
     }
 
     public String getPhone(){
