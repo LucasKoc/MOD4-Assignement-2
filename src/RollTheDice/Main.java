a2535eaa77522a0c0d98bd8def66b735c2113b3b
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-09-11 21:01:52 -0400

    Exercice 4 comment

--- src/RollTheDice/Main.java
+++ src/RollTheDice/Main.java
@@ -1,11 +1,17 @@
+/**
+ * Main class of RollTheDice package
+ * Will instantiate a frame from RollTheDice class.
+ * RollTheDice is a program that a dice that you can roll (1 to 6).
+ *
+ * @author SOLAN Tao & KOCOGLU Lucas
+ * @version N/A
+ * @since 2023-09-08
+ */
+
 package RollTheDice;
 
 public class Main {
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new RollTheDice();
-            }
-        });
+        new RollTheDice();
     }
 }
--- src/RollTheDice/RollTheDice.java
+++ src/RollTheDice/RollTheDice.java
@@ -1,7 +1,14 @@
 /**
+ * Class RollTheDice
+ * Generate a frame which will store an image of the dice and a button to roll the dice (1 to 6).
+ *
  * Credits assets :
  * - Dices image : Mike Zuidgeest on Flaticon (https://www.flaticon.com/authors/mike-zuidgeest)
  * - Dice icon : smashingstocks on Flaticon (https://www.flaticon.com/authors/smashingstocks)
+ *
+ * @author SOLAN Tao & KOCOGLU Lucas
+ * @version N/A
+ * @since 2023-09-08
  */
 
 package RollTheDice;
@@ -12,30 +19,39 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class RollTheDice extends JFrame {
+
+    // Window size
     private final int WINDOW_WIDTH = 275;
     private final int WINDOW_HEIGHT = 166;
 
-    private JButton rollTheDiceButton;
-    private ImageIcon[] diceImages;
-    private JLabel diceLabel1;
+    private JButton rollTheDiceButton; // Button to roll the dice
+    private ImageIcon[] diceImages; // Array of dices images
+    private JLabel diceLabel; // Label to store the image
 
+    /**
+     * Constructor of the class RollTheDice
+     * It set the frame parameters and load dices images, add an ActionListener and display the frame
+     */
     public RollTheDice() {
-        // Frame Parameters
+        // Frame Parameters : Title, window size, close operation and initial location of the window.
         setTitle("Roll the dice !");
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        // Dice images
-        diceImages = new ImageIcon[6];
+        // Store the path of images into the array diceImages
+        diceImages = new ImageIcon[6]; // Array of six ImageIcon
         for (int i = 0; i < 6; i++) {
+            // Images names is set to the dice number (ex: 1 -> dice1.png), images are stored into 'assets/png/diceX.png'
             diceImages[i] = new ImageIcon(getClass().getResource("assets/png/dice" + (i + 1) + ".png"));
         }
 
-        diceLabel1 = new JLabel("Please roll", SwingConstants.CENTER);
-        diceLabel1.setPreferredSize(new Dimension(128, 128));
+        // Settings of labels & buttons
+        diceLabel = new JLabel("Please roll", SwingConstants.CENTER);
+        diceLabel.setPreferredSize(new Dimension(128, 128));
         rollTheDiceButton = new JButton("Roll the Dice !");
 
+        // Action Listener, when the user click on the rollTheDiceButton, launch rollDice() function.
         rollTheDiceButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -43,20 +59,31 @@ public class RollTheDice extends JFrame {
             }
         });
 
+        // Display the frame
         setLayout(new FlowLayout());
-        add(diceLabel1);
+        add(diceLabel);
         add(rollTheDiceButton);
-
         setVisible(true);
     }
 
+    /**
+     * Function rollDice
+     * Will instantiate a variable from Random class
+     * Store a random number between 1 to 6
+     * Set the Image to the diceLabel (and remove its text)
+     * And resize the window to match the label size (by default, no needed because WINDOW_WIDTH/WINDOW_HEIGHT)
+     *  already at the good size.
+     */
     private void rollDice() {
+        // Instantiate random variable to generate the dice number
         Random random = new Random();
         int dice1Value = random.nextInt(6);
 
-        diceLabel1.setIcon(new ImageIcon(diceImages[dice1Value].getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
-        diceLabel1.setText(null);
+        // Catch the dice from diceImages(Array) and reshape the image to a 128*128 size.
+        diceLabel.setIcon(new ImageIcon(diceImages[dice1Value].getImage().getScaledInstance(128, 128, Image.SCALE_DEFAULT)));
+        diceLabel.setText(null);
 
+        // Pack will resize the window to match labels max width/height.
         pack();
     }
 }
